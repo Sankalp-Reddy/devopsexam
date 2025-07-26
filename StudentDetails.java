7bc7ca36e802bf910eaac7353a8a825f9c471277
Sankalp Reddy Cheemarla <245122737003@mvsrec.edu.in>
2025-07-26 10:29:03 +0530

    initial commit for exam

---
+++ StudentDetails.java
@@ -0,0 +1,12 @@
+public class StudentDetails { 
+    public static void main(String[] args) { 
+        String name = "Sankalp Reddy"; 
+        String rollNo = "245122737003"; 
+        String department = "INFORMATION TECHNOLOGY"; 
+        System.out.println("Student Details:"); 
+        System.out.println("Name : " + name); 
+	System.out.println("Roll No : " + rollNo); 
+	System.out.println("Department : " + department); 	
+} 
+} 
+
